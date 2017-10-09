8a7d6392232ee6d6b8b68004d2528a4551fcaca8
Santiago C <scas93@gmail.com>
2017-10-09 16:03:47 -0500

    se agrego el gulp y el gulpfile.js y el index.html

--- js/controllers/appController.js
+++
@@ -1,7 +0,0 @@
-var modules = angular.module('seed-app.appController', ['ngRoute']);
-
-modules.controller('AppCtrl', ['$scope', function ($scope) {
-    $scope.openModal = function() {
-        $('#modal1').openModal();
-    }
-}]);
